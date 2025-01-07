72fa6f1546f4d922c63ca9f122f09f6b9ec1e34d
Hoàng Quốc Phương <phuong.hoangquoc@outlook.com>
2025-01-07 15:06:09 +0700

    1st

--- SingleFoundation/obj/Debug/net8.0-windows/SingleFoundation.AssemblyInfo.cs
+++ SingleFoundation/obj/Debug/net8.0-windows/SingleFoundation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingleFoundation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8c75b78f3cff7df99f2657cb01c5f46df8d77a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingleFoundation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingleFoundation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
